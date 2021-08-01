3d1cbe2814ddd34d607cda2ca0cdf054e8713cb7
Shoya HARA <shioyarare@gmail.com>
2021-08-01 21:59:31 +0900

    initial commit.

---
+++ source/app.d
@@ -0,0 +1,27 @@
+import std.stdio;
+
+const int packet = 100_000; // 標本区間長
+const int n = 10; // 標本個数
+const int serv_rate = 100_000; // サービス率
+
+void main()
+{
+	// 課題1
+	calc1();
+
+	// 課題2
+	calc2();
+}
+
+// M/M/1 待ち行列システム
+void calc1()
+{
+	
+}
+
+
+// IPP/M/1/K 待ち行列システム
+void calc2()
+{
+
+}
